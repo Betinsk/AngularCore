5bc04619daafd485aafe76fe2b8239c1a4d64174
Betinsk <gmdust@hotmail.com>
2021-12-15 18:45:50 -0300

    a

--- src/app/venda/venda.component.ts
+++ src/app/venda/venda.component.ts
@@ -8,25 +8,37 @@ import { VendasService } from '../vendas/vendas.service';
 })
 export class VendaComponent implements OnInit {
   
-  vendas: any;
+  vendas: Array<any> = [];
   item: any;
   clientes: Array<any> = [];
   produtos: Array<any> = [];
-
+  
   constructor(private vendaService: VendasService) { }
 
   ngOnInit(): void {
-    this.listar()
     this.listarClientes()
+    
+    setTimeout(() => {
+      this.listar()
+    }, 1000);
+
   }
-    listar() {
-      this.vendaService.listar().subscribe(response => this.vendas = response )
+    async listar() {
+    await this.vendaService.listar().subscribe(response => {
+    this.vendas = response
+    this.vendas.forEach((e:any)  => {
+      let cliente = this.clientes.filter( y => y.id_cliente === e.id_cliente)[0]
+      e.cliente = cliente;
+    });
+    console.log(this.vendas)
+  })
+      
     }
 
     
-  listarClientes() {
-    this.vendaService.listarClientes()
-      .subscribe(response => this.clientes = response);
+    
+    async listarClientes() {
+      await this.vendaService.listarClientes().subscribe(response => this.clientes = response )
   }
 
 }
